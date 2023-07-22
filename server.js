c99317106baa926e93e3c2ce6e98bae650224481
Muhammad-abushanab <muhammad.7jehad@gmail.com>
2023-07-22 10:41:58 +0000

    modified files

--- server.js
+++ server.js
@@ -6,17 +6,11 @@ const qouteRoutes = require('./routes/api_routes/qoute_generator/api');
 
 require('dotenv').config();
 app.use(cors());
+
 const port = process.env.PORT;
 
 app.use(mainRoutes);
 app.use(qouteRoutes);
-// app.get('/moh',(req,res)=>{
-//     try {
-//         res.send('inside moh')
-//     } catch (error) {
-//         console.log(`error is ${error}`);
-//     }
-// })
 
 
 app.listen(port,() => {
