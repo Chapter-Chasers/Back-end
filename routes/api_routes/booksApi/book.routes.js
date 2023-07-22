5aaaced2ae4942c3275de4c2a9fc51e574ad84b5
Muhammad-abushanab <muhammad.7jehad@gmail.com>
2023-07-22 22:06:17 +0000

    now able to search on diffrent things

--- routes/api_routes/booksApi/book.routes.js
+++ routes/api_routes/booksApi/book.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const axios = require('axios');
 require('dotenv').config();
 
-const {BOOKS_API_URL} = require('../../../config');
+const { BOOKS_API_URL } = require('../../../config');
 
 //localhost:3000/allBooks
 router.get('/allBooks', async (req, res, next) => {
@@ -13,21 +13,39 @@ router.get('/allBooks', async (req, res, next) => {
         let axiosResponse = await axios.get(`${BOOKS_API_URL}get`)
         //res.send(axiosResponse.data.results)
         let books = axiosResponse.data;
-    
-        res.send(books)
+
+        res.status(200).send(books)
     } catch (e) {
         next('Failed to get all books' + e)
     }
-
-
 })
 //localhost:3000/search?search=bookName
 router.get('/search', async (req, res, next) => {
     try {
-        let bookName = req.body.title;
-        let axiosResponse = await axios.get(`${BOOKS_API_URL+bookName}`)
-        let search = axiosResponse.data;
-        res.send(search)
+        const bookName = req.body.title || req.query.isbn;
+        const axiosResponse = await axios.get(`${BOOKS_API_URL + encodeURIComponent(bookName)}`)
+        res.status(200).send(axiosResponse.data)
+
+    } catch (e) {
+        next('Cannot find book' + e)
+    }
+})
+router.get('/searchAuthor', async (req, res, next) => {
+    try {
+        const { author } = req.query;
+        const axiosResponse = await axios.get(`${BOOKS_API_URL}inauthor:${encodeURIComponent(author)}`)
+        res.status(200).send(axiosResponse.data)
+
+    } catch (e) {
+        next('Cannot find book' + e)
+    }
+})
+
+router.get('/searchCategory',async (req,res,next)=>{
+    try {
+        const { cat } = req.query;
+        const axiosResponse = await axios.get(`${BOOKS_API_URL}subject:${encodeURIComponent(cat)}`)
+        res.status(200).send(axiosResponse.data)
 
     } catch (e) {
         next('Cannot find book' + e)
