ec53b10ff029302e943c475018aa91a1824145fa
anshasiq <ahmadanshasi123@hotmail.com>
2023-07-23 11:41:45 +0000

    DB

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict'
-
+const clint=require('./clint');
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
 const mainRoutes = require('./routes/mainRoute');
-
+const DB_routes =require('./routes/database/DB')
 const bookRoutes = require('./routes/api_routes/booksApi/book.routes');
 
 const qouteRoutes = require('./routes/api_routes/qoute_generator/api');
@@ -22,12 +22,16 @@ app.use(express.json())
 app.use(mainRoutes);
 app.use(bookRoutes);
 app.use(qouteRoutes);
+app.use(DB_routes);
 
 
 app.use(pageNotFoundHandler);
 app.use(serverErrorHandler);
 
-
-app.listen(PORT, () => {
-    console.log(`server runs on ${PORT}`);
+clint.connect().then(() => {
+    app.listen(PORT, () => { console.log(`started at PORT ${PORT}`) })
 })
+
+// app.listen(PORT, () => {
+//     console.log(`server runs on ${PORT}`);
+// })
