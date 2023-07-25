4827a68ff54ef32a21c2d0ad7d196702973ac74a
Muhammad-abushanab <muhammad.7jehad@gmail.com>
2023-07-25 20:11:14 +0000

    updated route

--- routes/api_routes/booksApi/book.routes.js
+++ routes/api_routes/booksApi/book.routes.js
@@ -10,8 +10,7 @@ const { BOOKS_API_URL } = require('../../../config');
 //localhost:3000/allBooks
 router.get('/allBooks', async (req, res, next) => {
     try {
-        const word = wordGenerator();
-            let axiosResponse = await axios.get(`${BOOKS_API_URL+word}`)
+            let axiosResponse = await axios.get(`${BOOKS_API_URL}?q=Game`)
         //res.send(axiosResponse.data.results)
         let books = axiosResponse.data;
 
@@ -52,42 +51,4 @@ router.get('/searchCategory', async (req, res, next) => {
         next('Cannot find book' + e)
     }
 })
-const wordGenerator = () => {
-    const words = [
-        'Adventure',
-        'Romance',
-        'Mystery',
-        'Thriller',
-        'Science Fiction',
-        'Fantasy',
-        'Horror',
-        'Drama',
-        'History',
-        'Biography',
-        'Comedy',
-        'Self-Help',
-        'Cooking',
-        'Travel',
-        'Poetry',
-        'Art',
-        'Fiction',
-        'Non-Fiction',
-        'Memoir',
-        'Classics',
-        'Business',
-        'Health',
-        'Religion',
-        'Spirituality',
-        'Philosophy',
-        'Politics',
-        'Children',
-        'Young Adult',
-        'Graphic Novel',
-        'Manga',
-    ];
-
-    const randomIndex = Math.floor(Math.random() * words.length);
-    return words[randomIndex];
-};
-
 module.exports = router;
