911548114fe05aa33a89b8e8be5301347e9e2600
Muhammad-abushanab <muhammad.7jehad@gmail.com>
2023-07-26 10:08:30 +0000

    adding user route

--- routes/database/DB.js
+++ routes/database/DB.js
@@ -1,81 +1,120 @@
-const express = require ("express");
-const clint=require('../../clint');
+const express = require("express");
+const clint = require('../../clint');
 const router = express.Router();
 // const axios = require("axios");
 // const express = require("express");
 
 
 
-router.get ('/getbooks',(req,res,next)=>{
-    try{
+router.get('/getbooks', (req, res, next) => {
+  try {
     const sql = "SELECT * FROM table_one";
-    clint.query(sql).then((data)=>{
-        res.status(200).send(data.rows);
+    clint.query(sql).then((data) => {
+      res.status(200).send(data.rows);
 
-    }).catch((e)=> next("something went wrong" + e)); 
-    }
-    catch(e){
-        next(`Can't Get Books :( ${e}`);
+    }).catch((e) => next("something went wrong" + e));
+  }
+  catch (e) {
+    next(`Can't Get Books :( ${e}`);
 
-    }
+  }
 
 })
-router.post("/addbook",(req, res ,next) => {
-    try{
-  console.log("yout home");
-  let title=req.body.title; 
-  let image=req.body.image;
-  let description=req.body.description;
-  let rating=req.body.rating;
-  let price=req.body.price;
-  let author=req.body.author;
-  let category=req.body.category;
-  let yo = 'INSERT INTO table_one (title, image, description ,rating ,price,author,category) VALUES ($1, $2, $3, $4, $5, $6, $7)';
-  clint.query(yo, [title, image, description ,rating ,price,author,category]).then(() => {
-        res.status(201).send('book added succcful :)');
-      }).catch((e)=> next("something went wrong" + e));
-   
-  res.send(req.body);
+router.post("/addbook", (req, res, next) => {
+  try {
+    console.log("yout home");
+    let title = req.body.title;
+    let image = req.body.image;
+    let description = req.body.description;
+    let rating = req.body.rating;
+    let price = req.body.price;
+    let author = req.body.author;
+    let category = req.body.category;
+    let yo = 'INSERT INTO table_one (title, image, description ,rating ,price,author,category) VALUES ($1, $2, $3, $4, $5, $6, $7)';
+    clint.query(yo, [title, image, description, rating, price, author, category]).then(() => {
+      res.status(201).send('book added succcful :)');
+    }).catch((e) => next("something went wrong" + e));
+
+    res.send(req.body);
   }
-catch(e){
-next(`${e}`);
-}});
+  catch (e) {
+    next(`${e}`);
+  }
+});
+
+router.post('/user', (req, res, next) => {
+  try {
+    const { name, email, id } = req.body;
+    // check if users is exist
+    const sql = "select * from users where user_id = $1"
+    clint.query(sql, [id]).then((data) => {
+      console.log(data.rowCount);
+      if (data.rowCount > 0) {
+        res.status(200).send("user Exist");
+      }
+      else {
+        const sql2 = "insert into users (user_id , username , email ) values ($1 , $2 ,$3)";
+        clint.query(sql2, [id, name, email]).then(() => {
+          res.status(201).send('user Added Succesffully')
+        }).catch((e) => {
+          next(`error in adding user ${e}`);
+        })
+      }
+    }).catch((e) => {
+      next(`this is not allowed ${e}`)
+    })
+  } catch (error) {
+    next(`out of scoupe ${error}`)
+  }
+  
+})
 
 
 router.delete("/delete/:id", async (req, res) => {
-      try {
-        let { id } = req.params;
-        let sql = `DELETE FROM table_one WHERE id =${id}`;
-        let data = await clint.query(sql);
-        // console.log(data);
-        res.status(204).end();
-      } catch (e) {
-        next("deletebook " + e);
-      }
-    });
-
-    router.put("/UPDATE/:id", (req, res,next) => {
-        try {
-        console.log("asd");
-      let {id} = req.params;
+  try {
+    let { id } = req.params;
+    let sql = `DELETE FROM table_one WHERE id =${id}`;
+    let data = await clint.query(sql);
+    // console.log(data);
+    res.status(204).end();
+  } catch (e) {
+    next("deletebook " + e);
+  }
+});
+router.get("/getBook/:state", async (req, res) => {
+  try {
+    let { state } = req.params;
+    let sql = `SELECT * from table_one WHERE state = $1`;
+    clint.query(sql, [state]).then((data) => {
+      res.status(200).send(data.rows)
+    }).catch((e) => next("something went wrong" + e));
+  }
+  catch (e) {
+    next(`Can't Get Books :( ${e}`);
+  }
+})
+router.put("/UPDATE/:id", (req, res, next) => {
+  try {
+    console.log("asd");
+    let { id } = req.params;
     //   let title=req.body.title; 
     //   let image=req.body.image;
-      let rating=req.body.rating; //, image=2, description=$3 ,rating=$4,price=$5,author=$6,category=$7 WHERE
+    let rating = req.body.rating; //, image=2, description=$3 ,rating=$4,price=$5,author=$6,category=$7 WHERE
     //   let price=req.body.price;
     //   let author=req.body.author;
     //   let category=req.body.category;
-      let sql =  `UPDATE table_one SET rating=$1 Where id=${id}`;
-      clint.query(sql,[rating ]).then(() => {
-        res.status(200).send("updated successfully");
-      }).catch((e)=> next("something went wrong" + e));
-    }
-    catch (e) {
-        next("something went wrong" + e);
-      }
+    let sql = `UPDATE table_one SET rating=$1 Where id=${id}`;
+    clint.query(sql, [rating]).then(() => {
+      res.status(200).send("updated successfully");
+    }).catch((e) => next("something went wrong" + e));
+  }
+  catch (e) {
+    next("something went wrong" + e);
+  }
 
 });
 
 
 
 
-module.exports=router;
+module.exports = router;
