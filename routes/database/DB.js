b25cd67c4e4e461629f8c82056cb6be82b2615c0
Muhammad-abushanab <muhammad.7jehad@gmail.com>
2023-07-27 05:18:46 +0000

    edite state route

--- routes/database/DB.js
+++ routes/database/DB.js
@@ -74,9 +74,9 @@ router.post('/user', (req, res, next) => {
 
 router.get("/getBook/:state", async (req, res) => {
   try {
-    let { state } = req.params;
-    let sql = `SELECT * from table_one WHERE state = ${state}`;
-    clint.query(sql, [state]).then((data) => {
+    let { state ,id } = req.params;
+    let sql = `SELECT * from table_one WHERE state = $1 AND user_id = $2`;
+    clint.query(sql, [state,id]).then((data) => {
       res.status(200).send(data.rows)
     }).catch((e) => next("something went wrong" + e));
   }
