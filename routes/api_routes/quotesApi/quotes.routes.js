fdfc146b3ea74d580e524ab67dccec7cd9a7dbf5
Muhammad-abushanab <muhammad.7jehad@gmail.com>
2023-07-29 14:58:38 +0000

    editing addquotes route

--- routes/api_routes/quotesApi/quotes.routes.js
+++ routes/api_routes/quotesApi/quotes.routes.js
@@ -5,33 +5,35 @@ const router = express.Router();
 const axios = require('axios');
 
 const { QOUTEPAGE_API_URL } = require('../../../config');
+const client = require('../../../clint');
 
 //localhost:3000/allQuotes
 router.get('/allQuotes', async (req, res) => {
-    try {
-        const response = await axios.get(process.env.QOUTEPAGE_API_URL);
-        const quotes = response.data;
-        res.status(200).json(quotes);
-      } catch (error) {
-        console.error('Error fetching quotes:', error);
-        res.status(500).json({ error: 'An error occurred while fetching quotes.' });
-      }
-  });
+  try {
+    const response = await axios.get(process.env.QOUTEPAGE_API_URL);
+    const quotes = response.data;
+    res.status(200).json(quotes);
+  } catch (error) {
+    console.error('Error fetching quotes:', error);
+    res.status(500).json({ error: 'An error occurred while fetching quotes.' });
+  }
+});
 
-  router.post('/addQuotes', async (req, res, next) => {
-    try {
-      const id= req.body.id;
-      const author = req.body.author;
-      const quote = req.body.quote;
-       const sql= 'INSERT INTO quotes(userId,author,quote) VALUES ($1,$2,$3) ';
-       clint.query(sql, [id,author,quote]).then(() => {
-        res.status(201).send('quote added succcfuly :)'); 
-      }).catch((error) => next("something went wrong" + error));
-
-      } catch (error) {
-       next(error)
-      }
-  });
+router.post('/addQuotes', async (req, res, next) => {
+  try {
+    const { id, author, qoute } = req.body;
+    const sql = "insert into quotes (userid , author , quote) values ($1 , $2 , $3)";
+    client.query(sql, [id, author, qoute]).then(() => {
+      res.status(201).send("successfully created quote")
+    }).catch((e) => {
+      next(`error is ${e}`)
+    })
+  }
+  catch (error) {
+    console.error('Error fetching quotes:', error);
+    res.status(500).json({ error: 'An error occurred while fetching quotes.' });
+  }
+});
 
 
 
